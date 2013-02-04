0fb234419c1635ebfdf9b8817d4d499bbd3ef9c8
Steven Lonergan <sdlonergan@gmail.com>
2013-02-03 22:43:52 -0700

    updated routing and layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,17 +6,17 @@ describe "Static pages" do
 
   describe "Home page" do 
     it "should have the content 'Sample App'" do 
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content('Sample App')
     end 
 
     it "Should have the base title" do 
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
     end
 
     it "Should have the right title" do 
-      visit '/static_pages/home' 
+      visit root_path 
       page.should_not have_selector('title', :text => "#{base_title} | Home")
     end
 
@@ -24,12 +24,12 @@ describe "Static pages" do
 
   describe "Help Page" do 
     it "Should have the content 'Help'" do 
-      visit '/static_pages/help'
+      visit help_path      
       page.should have_content('Help')
     end
 
     it "Should have the right title" do 
-      visit '/static_pages/help' 
+      visit help_path 
       page.should have_selector('title', :text => "#{base_title} | Help")
     end
 
@@ -37,24 +37,24 @@ describe "Static pages" do
 
   describe "About Page" do 
     it "Should have the content 'About Us'" do 
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About Us')
     end
 
     it "Should have the right title" do 
-      visit '/static_pages/about' 
+      visit about_path
       page.should have_selector('title', :text => "#{base_title} | About Us")
     end
   end
 
   describe "Contact Page" do 
     it "Should have the content 'Contact Us'" do 
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_content('Contact Us')
     end 
 
     it "Should have the right title" do 
-      visit '/static_pages/contact' 
+      visit contact_path
       page.should have_selector('title', :text => "#{base_title} | Contact Us")
     end
   end
