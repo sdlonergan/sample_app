0fb1a9c0d8c14478e42943dbc52652bf49d52c9c
Steven Lonergan <sdlonergan@gmail.com>
2013-02-05 13:04:14 -0700

    Finished user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do 
+    name                    "Steven Lonergan"
+    email                   "sdlonergan@gmail.com"
+    password                "foobar"
+    password_confirmation   "foobar"
+  end 
+end
